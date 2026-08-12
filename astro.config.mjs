74f8d3d5b4753434f6e67072321c5e32e23397fa
Trideep Dutta <trideepdutta@Trideeps-Mac-mini.local>
2026-08-12 23:06:34 +0530

    CMS update — 8/12/2026, 11:06:34 PM

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   site: 'https://getusranked.com',
   integrations: [
     sitemap({
-      filter: (page) => !page.includes('/403') && !page.includes('/404') && !page.includes('/500'),
+      filter: (page) => !page.includes('/403') && !page.includes('/404') && !page.includes('/500') && !page.includes('/admin'),
       changefreq: 'weekly',
       priority: 0.8,
       lastmod: new Date(),
